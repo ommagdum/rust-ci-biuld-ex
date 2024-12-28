026b8abd5f152b2a2e46edf70f0b4d7139072422
Om Magdum <117524713+ommagdum@users.noreply.github.com>
2024-12-28 19:09:01 +0000

    ready for CI

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+// Run All Functions in lib.rs
+
+use trust::{add, div, mul, sub};
+
+fn main() {
+    println!("Hello, world!");
+    println!("add(1, 2) = {}", add(1, 2));
+    println!("sub(1, 2) = {}", sub(1, 2));
+    println!("mul(1, 2) = {}", mul(1, 2));
+    println!("div(1, 2) = {}", div(1, 2));
+}
